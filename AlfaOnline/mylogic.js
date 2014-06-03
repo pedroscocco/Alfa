9b4264e5f8a5a76aa8ad7ac4d3b59a527ae0ae2d
Aline Borges <Aline Borges@Aline>
2014-06-02 22:36:41 -0300

    Alteração das funções JS

--- AlfaOnline/mylogic.js
+++ AlfaOnline/mylogic.js
@@ -1,4 +1,7 @@
 var letrasVistas=0;
+var letterCenter;
+var correctLetter;
+var answer;
 
 function mostrarPopup(object1, object2, object3, object4) {
     var source1=object1;
@@ -13,5 +16,38 @@ function mostrarPopup(object1, object2, object3, object4) {
     }
 
 }
+
+function setLetterCenter(source){
+   letterCenter=source;
+}
+
+function setCorrectLetter(source){
+    if(source1.indexOf("AHeader")>0){
+        correctLetter='A_.png';
+    }else  if(source1.indexOf("BHeader")>0){
+        correctLetter='B_.png';
+    }else  if(source1.indexOf("CHeader")>0){
+        correctLetter='C_.png';
+    }else  if(source1.indexOf("DHeader")>0){
+        correctLetter='D_.png';
+    }
+}
+
+
+function getSource(){
+    if(correctLetter!==null && letterCenter!==null && answer!==null){
+        if(answer.indexOf(correctLetter)===0){
+            return answer.replace('_', 'Ok');
+        }else{
+            return answer.replace('_', 'Wrong');
+        }
+    } else return letterCenter;
+}
+
+function setAnswer(){
+    answer=letterCenter;
+}
+
+
 
 
